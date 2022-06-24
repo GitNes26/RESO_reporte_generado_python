0b4f6bf0bb0ebbf75bdd09aaeb4520526b2180e9
GitNes26 <nestorpuentesin@gmail.com>
2022-06-24 16:11:53 -0500

    actualizcion

---
+++ Classes/TableEmail.py
@@ -0,0 +1,12 @@
+from pymysql import NULL
+
+
+class RegisterTableEmail:
+   def __init__(self):
+      self.tableTitle = NULL
+      self.EtiquetaDeFila = NULL
+      self.PromedioPreg = NULL
+
+class TableEmail:
+   def __init__(self):
+      self.Registers = []
